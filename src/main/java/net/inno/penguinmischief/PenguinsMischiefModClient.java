434fe850af6560b8d7533304698e8d3fea422e0e
Innocentzz <InnocentLIVE24@gmail.com>
2023-08-12 21:07:56 +0200

    helo

---
+++ src/main/java/net/inno/penguinmischief/PenguinsMischiefModClient.java
@@ -0,0 +1,10 @@
+package net.inno.penguinmischief;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PenguinsMischiefModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/inno/penguinmischief/PenguinsMischiefModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.inno.penguinmischief;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PenguinsMischiefModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
